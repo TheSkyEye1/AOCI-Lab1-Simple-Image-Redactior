ede9da9cdda0bddb4a8371f06b92c80474868755
Сергей Голованчиков <golovan4ickoff@gmail.com>
2025-11-24 16:52:58 +0700

    Загрузка лабораторной

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+//Пространства имен которые необходимо добавить
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace aoci_lab1
+{
+    public partial class MainWindow : Window
+    {
+        //Главный объект для работы с изображением в Emgu.CV. Этот обьект должен быть обьявлен глобально в проекте
+        //Это поле будет хранить наше оригинальное изображение для всех операций.
+        private Image<Bgr, byte> sourceImage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Методы-конвертеры
+
+        //Функция конвертирует изображение из формата Emgu.CV (Image<Bgr, byte>) в формат, понятный для WPF (BitmapSource).
+        //Это необходимо, чтобы мы могли отобразить результат обработки в элементе Image на нашем окне.
+        public BitmapSource ToBitmapSource(Image<Bgr, byte> image)
+        {
+            //У каждого объекта Image<,> есть свойство .Mat, которое предоставляет
+            //доступ к матрице пикселей.
+            var mat = image.Mat;
+
+            return BitmapSource.Create(
+                mat.Width,
+                mat.Height,
+                96d, //Горизонтальное разрешение (DPI), 96 - стандарт для экранов
+                96d, //Вертикальное разрешение (DPI)
+                PixelFormats.Bgr24, //Мы указываем WPF, что данные идут в формате Bgr, по 24 бита на пиксель (8 бит на синий, 8 на зеленый, 8 на красный).
+                null, //Палитра не используется для 24-битных изображений
+                mat.DataPointer, //Указатель на начало данных изображения в памяти.
+                mat.Step * mat.Height, //Общий размер буфера данных в байтах.
+                mat.Step); // Шаг - это длина одной строки изображения в байтах.
+        }
+
+        //Функция конвертирует изображение из формата WPF (BitmapSource) обратно в формат Emgu.CV (Image<Bgr, byte>).
+        public Image<Bgr, byte> ToEmguImage(BitmapSource source)
+        {
+            if (source == null) return null;
+
+            //Чтобы гарантировать, что у нас есть данные в формате Bgr24, мы создаем "конвертер" FormatConvertedBitmap.
+            FormatConvertedBitmap safeSource = new FormatConvertedBitmap();
+            safeSource.BeginInit();
+            safeSource.Source = source;
+            safeSource.DestinationFormat = PixelFormats.Bgr24; //Явно указываем нужный нам формат
+            safeSource.EndInit();
+
+            //Создаем пустое изображение Emgu.CV нужного размера.
+            Image<Bgr, byte> resultImage = new Image<Bgr, byte>(safeSource.PixelWidth, safeSource.PixelHeight);
+            var mat = resultImage.Mat;
+
+            //Копируем пиксели из WPF-изображения (safeSource) напрямую в память нашего Emgu.CV изображения (resultImage).
+            safeSource.CopyPixels(
+                new System.Windows.Int32Rect(0, 0, safeSource.PixelWidth, safeSource.PixelHeight), //Какую область копировать
+                mat.DataPointer, //Куда копировать (в начало данных матрицы Emgu.CV)
+                mat.Step * mat.Height, //Размер буфера назначения
+                mat.Step); //Шаг в буфере назначения
+
+            return resultImage;
+        }
+
+
+        //Обработчики событий от элементов UI
+
+        //Функция загрузки изображения
+        private void LoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы изображений (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                //Cоздаем объект Image<Bgr, byte> напрямую из пути к файлу, Emgu.CV сам его загрузит и декодирует.
+                sourceImage = new Image<Bgr, byte>(openFileDialog.FileName);
+                //Конвертируем наше Emgu.CV изображение в понятный для WPF формат с помощью нашего конвертера.
+                MainImage.Source = ToBitmapSource(sourceImage);
+            }
+        }
+
+        //Функция сохранения изображения
+        private void SaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            //Получаем текущее отображаемое изображение из элемента MainImage.
+            BitmapSource currentWpfImage = MainImage.Source as BitmapSource;
+            if (currentWpfImage == null)
+            {
+                MessageBox.Show("Отсутсвует изображение");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG Image (*.png)|*.png|JPEG Image (*.jpg)|*.jpg|Bitmap Image (*.bmp)|*.bmp";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+
+                    //Перед сохранением конвертируем изображение из WPF-формата в Emgu.CV.
+                    Image<Bgr, byte> imageToSave = ToEmguImage(currentWpfImage);
+
+                    //У Emgu.CV есть удобный встроенный метод .Save()
+                    imageToSave.Save(saveFileDialog.FileName);
+
+                    MessageBox.Show($"Изображение успешно сохранено в {saveFileDialog.FileName}");
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show($"Ошибка! Не могу сохранить файл. Подробности: {ex.Message}");
+                }
+            }
+        }
+
+
+        //Функция инвертации значения пикселей
+        private void Invert_Click(object sender, RoutedEventArgs e)
+        {
+            //Всегда проверяем, загружено ли изображение.
+            if (sourceImage == null) return;
+
+            //ВАЖНО: Мы не хотим изменять оригинальное изображение (sourceImage).
+            //Вместо этого мы создаем его точную копию (клон) и работаем с ней.
+            Image<Bgr, byte> invertedImage = sourceImage.Clone();
+
+            //Проходим по каждому пикселю изображения.
+            for (int y = 0; y < invertedImage.Rows; y++)
+            {
+                for (int x = 0; x < invertedImage.Cols; x++)
+                {
+
+                    //Получаем доступ к пикселю по его координатам (y, x).
+                    //Emgu.CV возвращает структуру Bgr, у которой есть поля .Blue, .Green, .Red.
+                    Bgr pixel = invertedImage[y, x];
+
+                    // Инвертируем каждый цветовой канал. Максимальное значение - 255.
+                    pixel.Blue = 255 - pixel.Blue;
+                    pixel.Green = 255 - pixel.Green;
+                    pixel.Red = 255 - pixel.Red;
+
+                    //Записываем измененный пиксель обратно в изображение.
+                    invertedImage[y, x] = pixel;
+                }
+            }
+
+            //Отображаем результат в окне.
+            MainImage.Source = ToBitmapSource(invertedImage);
+        }
+        //Функция изменения яркости изображения
+        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (sourceImage == null) return;
+
+            Image<Bgr, byte> lightImage = sourceImage.Clone();
+
+            // Получаем новое значение со слайдера.
+            int brightness = (int)e.NewValue;
+
+            for (int y = 0; y < lightImage.Rows; y++)
+            {
+                for (int x = 0; x < lightImage.Cols; x++)
+                {
+                    Bgr pixel = lightImage[y, x];
+
+                    //Прибавляем значение яркости к каждому каналу.
+                    int b = (int)pixel.Blue + brightness;
+                    int g = (int)pixel.Green + brightness;
+                    int r = (int)pixel.Red + brightness;
+
+                    //Результат сложения может выйти за пределы 0-255.
+                    //Поэтому результат зажимается допустимом диапазоне.
+                    pixel.Blue = (byte)Math.Max(0, Math.Min(255, b));
+                    pixel.Green = (byte)Math.Max(0, Math.Min(255, g));
+                    pixel.Red = (byte)Math.Max(0, Math.Min(255, r));
+
+                    lightImage[y, x] = pixel;
+                }
+            }
+            MainImage.Source = ToBitmapSource(lightImage);
+        }
+    }
+}
